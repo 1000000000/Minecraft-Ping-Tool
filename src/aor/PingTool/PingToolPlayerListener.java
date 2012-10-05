7150f1320960a784a56e03b72e09e8800eb901c8
1000000000 <lthyde@gmail.com>
2012-10-05 12:59:53 -0400

    Now fakes block changes instead of changing blocks, Needs Testing!

--- src/aor/PingTool/PingToolPlayerListener.java
+++ src/aor/PingTool/PingToolPlayerListener.java
@@ -3,8 +3,8 @@ package aor.PingTool;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,16 +15,18 @@ import org.bukkit.material.MaterialData;
 
 public class PingToolPlayerListener implements Listener {	
 	public static PingTool plugin;
-	public static List<Block> replacedBlocks=new ArrayList<Block>();
-	public static List<BlockState> replacedBlocksState=new ArrayList<BlockState>();
+	public static List<Location> replacedBlocks=new ArrayList<Location>();
+	//public static List<BlockState> replacedBlocksState=new ArrayList<BlockState>();
 
 	public PingToolPlayerListener(PingTool instance) {
 		plugin = instance;
 	}
 
+	@Deprecated
+	@SuppressWarnings("all")
 	public static void replaceBlock(){
 		//put the block back in world
-		BlockState blockState = replacedBlocksState.remove(0);
+		BlockState blockState = null;//replacedBlocksState.remove(0);
 		if(!replacedBlocks.contains(replacedBlocks.remove(0))){
 			blockState.update(true); //forces existing block to become the block represented by blockState
 		}
@@ -37,25 +39,27 @@ public class PingToolPlayerListener implements Listener {
 		// Left clicking air or a block event:
 		if ((event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) && itemInHand.getItemType() == Material.WOOL) // If they left clicked with wool.
 		{
-			Block targetBlock = player.getTargetBlock(null, 1000); // Select the target block.
-			if (targetBlock.getType() != Material.AIR) // No pinging midair!
+			Location targetBlock = player.getTargetBlock(null, 1000).getLocation(); // Select the target block.
+			if (targetBlock.getBlock().getType() != Material.AIR) // No pinging midair!
 			{
 				event.setCancelled(true);
 				if(!replacedBlocks.contains(targetBlock)){
-					replacedBlocks.add(targetBlock);//store the block
-					replacedBlocksState.add(targetBlock.getState());
+					replacedBlocks.add(targetBlock);//store the block location
+					//replacedBlocksState.add(targetBlock.getState());
 				}
 				else{
 					int replacedBlockIndex = replacedBlocks.indexOf(targetBlock);
 					replacedBlocks.add(replacedBlocks.get(replacedBlockIndex));//store the block
-					replacedBlocksState.add(replacedBlocksState.get(replacedBlockIndex));
+					//replacedBlocksState.add(replacedBlocksState.get(replacedBlockIndex));
 				}
-				targetBlock.setTypeIdAndData(itemInHand.getItemTypeId(), itemInHand.getData(), false); // Turn it to the wool player was holding!
-				plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-					public void run() {
-						replaceBlock();
-					}
-				}, 20L);
+				for(Player p : plugin.getServer().getOnlinePlayers()) {
+					p.sendBlockChange(targetBlock, itemInHand.getItemTypeId(), itemInHand.getData()); // Turn it to the wool player was holding!
+					/*plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+						public void run() {
+							replaceBlock();
+						}
+					}, 20L);*/
+				}
 			}
 		}
 	}
