55049f4ac70f19297b27cc325d44d3d9cda5867d
1000000000 <lthyde@gmail.com>
2013-04-12 16:31:07 -0400

    PingTool.java now controls pinging rather than PingToolPlayerListener

--- src/aor/PingTool/PingToolPlayerListener.java
+++ src/aor/PingTool/PingToolPlayerListener.java
@@ -1,8 +1,5 @@
 package aor.PingTool;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -15,26 +12,11 @@ import org.bukkit.material.MaterialData;
 
 public class PingToolPlayerListener implements Listener {	
 	public static PingTool plugin;
-	public static List<Location> replacedBlocks=new ArrayList<Location>();
 	public static final boolean ALLOW_PING_MIDAIR = true;
 	//public static List<BlockState> replacedBlocksState=new ArrayList<BlockState>();
 
 	public PingToolPlayerListener(PingTool instance) {
 		plugin = instance;
-	}
-
-	public static void replaceBlock(){
-		Location pingLocation = replacedBlocks.remove(0);
-		if(!replacedBlocks.contains(pingLocation)){
-			pingLocation.getBlock().getState().update(false); //forces existing block to become the block represented by blockState
-		}
-	}
-	
-	public static void updateBlock() {
-		Location blockLoc = replacedBlocks.remove(0);
-		if(!replacedBlocks.contains(blockLoc)){
-			blockLoc.getBlock().getState().update();
-		}
 	}
 
 	@EventHandler
@@ -52,43 +34,8 @@ public class PingToolPlayerListener implements Listener {
 			if (targetBlock.getBlock().getType() != Material.AIR || ALLOW_PING_MIDAIR) // No pinging midair! (unless you can ping midair)
 			{
 				event.setCancelled(true);
-				pingBlock(plugin.getServer().getOnlinePlayers(), targetBlock, Material.WOOL, ((Dye)itemInHand).getColor().getWoolData(),  20L);
+				plugin.pingBlock(plugin.getServer().getOnlinePlayers(), targetBlock, Material.WOOL, ((Dye)itemInHand).getColor().getWoolData(),  20L);
 			}
-		}
-	}
-	
-	public void pingBlock(final Player[] hallucinators, final Location blockLocation, final Material material, final byte data, long fakeBlockTimeSpan) {
-		fakeBlock(hallucinators, blockLocation, material, data);
-		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-			public void run() {
-				replaceBlock();
-			}
-		}, fakeBlockTimeSpan);
-		if(!replacedBlocks.contains(blockLocation)){
-			replacedBlocks.add(blockLocation);//store the block location
-		}
-		else{
-			int replacedBlockIndex = replacedBlocks.indexOf(blockLocation);
-			replacedBlocks.add(replacedBlocks.get(replacedBlockIndex));//store the block
-		}
-	}
-	
-	/**
-	 * This causes a block that does not necessarily exist at
-	 * blockLocation to seem to exist there by players in the
-	 * array of players given until a block update occurs.
-	 * 
-	 * This differs from pingBlock() by not forcing an update
-	 * after a certain amount of time has passed so a faked
-	 * block can remain indefinitely
-	 */
-	public void fakeBlock(Player[] hallucinators, Location blockLocation, Material material, byte data) {
-		for(Player p : hallucinators) {
-			if(!p.isOnline()) {
-				plugin.getLogger().fine("[PingPlugin] Trying to hallucinate to Player " + p + " who is not online");
-				continue;
-			}
-			p.sendBlockChange(blockLocation, material, data); // Turn it to the given block
 		}
 	}
 }
